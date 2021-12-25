8516dc5614ecbea8d864259b5d1446c8f116e55a
jopetty <jacksonopetty@gmail.com>
2021-12-25 02:13:22 +0000

    Add template train, eval methods

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,8 +1,24 @@
+import logging
+import warnings
 from typing import Sequence
 
 import rich.syntax
 import rich.tree
 from omegaconf import DictConfig, OmegaConf
+
+
+def set_all_seeds(seed: int, workers: bool = True):
+
+    raise ValueError("Please ensure that random seeds are properly set.")
+
+
+def get_logger(name=__name__) -> logging.Logger:
+
+    logger = logging.getLogger(name)
+
+    warnings.warn("Please ensure that loggers are not duplicated across GPUs.")
+
+    return logger
 
 
 def print_config(
