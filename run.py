64167f378c7f2a40adc4b577c5d6ba6f64c194e7
jopetty <jacksonopetty@gmail.com>
2021-12-25 01:03:39 +0000

    Initial commit

---
+++ run.py
@@ -0,0 +1,17 @@
+import dotenv
+import hydra
+from omegaconf import DictConfig
+
+dotenv.load_dotenv(override=True)
+
+@hydra.main(config_path="conf/", config_name="config.yaml")
+def main(cfg: DictConfig):
+
+    from src.utils import utils
+
+    if cfg.get("print_config"):
+        utils.print_config(cfg, resolve=True)
+    
+
+if __name__ == "__main__":
+    main()
---
+++ src/utils/utils.py
@@ -0,0 +1,36 @@
+from typing import Sequence
+
+import rich.syntax
+import rich.tree
+from omegaconf import DictConfig, OmegaConf
+
+
+def print_config(
+    cfg: DictConfig,
+    fields: Sequence[str] = (
+        "trainer",
+        "model",
+        "datamodule",
+        "callbacks",
+        "logger",
+        "test_after_training",
+        "seed",
+        "name",
+    ),
+    resolve: bool = True,
+) -> None:
+    tree = rich.tree.Tree("CONFIG")
+
+    for field in fields:
+        branch = tree.add(field)
+        config_section = cfg.get(field)
+        branch_content = str(config_section)
+        if isinstance(config_section, DictConfig):
+            branch_content = OmegaConf.to_yaml(config_section, resolve=resolve)
+
+        branch.add(rich.syntax.Syntax(branch_content, "yaml"))
+
+    rich.print(tree)
+
+    with open("config_tree.log", "w") as fp:
+        rich.print(tree, file=fp)
